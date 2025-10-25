94a15cbcf39d35c50fd48664f56a49e38e8cb476
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-10-25 15:23:40 +0500

    Item Ar Name and With VAT Rage

--- posar/public/js/item.js
+++ posar/public/js/item.js
@@ -8,20 +8,35 @@ frappe.ui.form.on('Item', {
                 callback: function(r) {
                     if (r.message) {
                         frm.set_value('item_name_ar', r.message);
+                        frm.set_value('item_name', frm.doc.item_code);
                     }
                 }
             });
         }
     },
 
-    final_price: function(frm) {
+    standard_rate: function(frm) {
         discount_perc = frm.doc.discount_percentage || 15
+        with_vat_price = frm.doc.standard_rate;
 
-        if (frm.doc.final_price ) {
-            let discount_factor = 1 - (frm.doc.discount_percentage / 100);
+        if (with_vat_price && discount_perc) {
+            let discount_factor = with_vat_price * discount_perc / 100;
             if (discount_factor > 0) {
-                const calculated_price = (frm.doc.final_price / discount_factor).toFixed(2);
-                frm.set_value('standard_rate', calculated_price);
+                const calculated_price = with_vat_price - discount_factor;
+                frm.set_value('custom_selling_rate_without_vat', calculated_price);
+                frappe.show_alert({ message: __("Base price calculated: ") + calculated_price, indicator: "green" });
+            }
+        }
+    },
+    valuation_rate: function(frm) {
+        discount_perc = frm.doc.discount_percentage || 15
+        with_vat_price = frm.doc.valuation_rate;
+
+        if (with_vat_price && discount_perc) {
+            let discount_factor = with_vat_price * discount_perc / 100;
+            if (discount_factor > 0) {
+                const calculated_price = with_vat_price - discount_factor;
+                frm.set_value('custom_valuation_rate_without_vat', calculated_price);
                 frappe.show_alert({ message: __("Base price calculated: ") + calculated_price, indicator: "green" });
             }
         }
